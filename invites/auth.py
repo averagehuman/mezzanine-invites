a44f829e3fe14262b34f27281503942ed70d1d63
averagehuman <cyclebelfast@gmail.com>
2014-09-25 21:00:19 +0100

    update auth backend and management command to match refactored model

--- invites/auth.py
+++ invites/auth.py
@@ -1,5 +1,5 @@
 
-from datetime import datetime
+from django.utils import timezone
 from django.db import models
 from django.contrib.sites.models import Site
 from django.contrib.auth import get_user_model
@@ -8,10 +8,9 @@ from django.utils.translation import ugettext_lazy as _
 from django.utils.crypto import get_random_string
 from django.core.exceptions import ImproperlyConfigured, PermissionDenied
 
+from mezzanine.conf import settings
 
 from .models import InvitationCode
-
-User = get_user_model()
 
 class InviteAuthBackend(object):
 
@@ -20,19 +19,26 @@ class InviteAuthBackend(object):
             invite_key = kwargs.pop("invite_key", None)
             email = kwargs.pop("email", None)
             if invite_key:
-                code = InvitationCode.objects.get_code_from_key_if_valid(invite_key, email)
+                code = InvitationCode.objects.get_code_from_key_if_valid(
+                    invite_key, email
+                )
                 if code:
-                    if not code.registered_by:
-                        site = Site.objects.get_current().pk
-                        user = User.objects.create_user(
-                            '%s%05d' % (site, code.pk+40)
+                    User = get_user_model()
+                    email = code.registered_to
+                    try:
+                        user = User.objects.get(username=email)
+                    except User.DoesNotExist:
+                        name = (code.registered_name or '').partition(' ')
+                        user= User.objects.create_user(
+                            email, email, first_name=name[0], last_name=name[1],
+                            phone=code.registered_phone,
                         )
-                        code.registered_by = user
-                        code.registered_date = datetime.now()
+                    if not code.registered_date:
+                        code.registered_date = timezone.now()
                         if getattr(settings, 'INVITE_CODES_ARE_REUSABLE', True) is False:
                             code.expired = True
                         code.save()
-                    return code.registered_by
+                    return user
         return None
 
     def get_user(self, user_id):
--- invites/management/commands/invite.py
+++ invites/management/commands/invite.py
@@ -16,34 +16,84 @@ from django.contrib.sites.models import Site
 from django.utils.six.moves import input
 
 from invites.models import InvitationCode
+from invites.utils import send_invite_code_mail
 
 class Command(BaseCommand):
 
     option_list = BaseCommand.option_list + (
-        make_option('--domain', dest='domain',
-            help="The site's url root domain"),
         make_option('--email', dest='email',
             help="The email address of the invitee"),
+        make_option('--domain', dest='domain',
+            help="The site's url root domain"),
         make_option('--database', action='store', dest='database',
-            default=DEFAULT_DB_ALIAS, help='Specifies the database to use. Default is "default".'),
+            default=DEFAULT_DB_ALIAS,
+            help='Specifies the database to use. Default is "default".'),
     )
     help = 'Create an Invite Code'
 
     def handle(self, *args, **options):
-        domain = options.get('domain')
-        if not domain:
-            site = Site.objects.get_current()
-        else:
-            try:
-                site = Site.objects.get(domain=domain)
-            except Site.DoesNotExist:
-                raise CommandError("A site with domain '%s' does not exist" % domain)
-        email = options.get('email')
-        while not email:
-            email = input(force_str('Email: '))
         verbosity = int(options.get('verbosity', 1))
         database = options.get('database')
+        email = options.get('email')
+        domain = options.get('domain')
+        sites = Site.objects.all()
+        site_items = dict((str(obj.id), obj) for obj in sites)
+        site = None
+        if not site_items:
+            raise CommandError("No site available.")
+        while not email:
+            email = (input(
+                force_str('Enter the email address of the recipient: ')
+            ) or '').strip()
+        if domain:
+            for site in sites:
+                if site.domain == domain:
+                    break
+            else:
+                raise CommandError("A site with domain '%s' does not exist" % domain)
+        else:
+            if len(site_items) == 1:
+                site = sites[0]
+            while site is None:
+                for obj in sites:
+                    self.stdout.write("[%s] %s" % (obj.id, obj.domain))
+                site_id = input(force_str('Select a site: '))
+                try:
+                    site = site_items[site_id]
+                except KeyError:
+                    continue
         code = InvitationCode.objects.create_invite_code(email, site=site)
-        self.stdout.write(code.short_key)
+        do_send = None
+        while do_send is None:
+            do_send = (input(
+                force_str('Send the code now? [y/N]')
+            ) or '').strip().lower()
+            do_send = do_send or 'n'
+            if do_send not in 'yn':
+                do_send = None
+        if do_send == 'y':
+            secure = None
+            while secure is None:
+                secure = (input(
+                    force_str('Use https for site urls in the email? [y/N]')
+                ) or '').strip().lower()
+                secure = secure or 'n'
+                if secure not in 'yn':
+                    secure = None
+            protocol = 'http'
+            if secure == 'y':
+                protocol += 's'
+            site_url = '%s://%s%s' % (protocol, site.domain, reverse("home"))
+            login_url = '%s://%s%s' % (protocol, site.domain, reverse("login"))
+            try:
+                send_invite_code_mail(code, site_url, login_url)
+            except Exception as e:
+                self.stdout.write("Mail send error - %s" % e)
+                sys.exit("FAIL")
+                return
+            else:
+                self.stdout.write("Mail sent to %s." code.registered_to)
+        else:
+            self.stdout.write(code.short_key)
 
 
