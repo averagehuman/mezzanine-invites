707842f5ac368a4278dc0fbfb363828cf374809b
averagehuman <cyclebelfast@gmail.com>
2014-09-28 18:04:43 +0100

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 class Tox(TestCommand):
     def finalize_options(self):
